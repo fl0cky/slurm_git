1ff8aebb9c3aadfe707cbd5fdf1f3201b2df75a9
fl0cky <sairusflo@yandex.ru>
2022-08-18 17:42:42 +0300

    tst

--- dz_2_2.py
+++ dz_2_2.py
@@ -1,7 +1,7 @@
 from typing import Generator
 
 
-def my_range(stop: float, start=0.0, step=1.0) -> Generator[float]:
+def my_range(stop: float, start=0.0, step=1.0) -> Generator:
     while start <= stop:
         yield start
         start += step
